0d8e0f4d2f52cd1ae139f480bb239fc52b558888
saeed1202 <saeed1202@users.noreply.github.com>
2024-03-06 18:58:27 +0330

    update: timer in functional

--- app/(landing)/timer.tsx
+++ app/(landing)/timer.tsx
@@ -2,8 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { TimerIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { differenceInSeconds, secondsToHours } from "date-fns";
-import { START } from "@/lib/consts";
+import { addDays, differenceInSeconds, endOfDay, startOfDay } from "date-fns";
+import { END, START } from "@/lib/consts";
 
 type TimerPartProps = {
   time: number;
@@ -38,8 +38,11 @@ function TimeDivider({ className }: React.HTMLAttributes<HTMLDivElement>) {
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const started = Date.now() > START;
+
   useEffect(() => {
-    setTime(differenceInSeconds(START, Date.now()));
+    if (!started) setTime(differenceInSeconds(START, Date.now()));
+    else setTime(differenceInSeconds(END, Date.now()));
     const interval = setInterval(() => {
       setTime((t) => --t);
     }, 1000);
@@ -51,15 +54,13 @@ const Timer = () => {
   const minutes = (time / 60) % 60;
   const hours = (time / 60 / 60) % 24;
   const days = (time / 60 / 60 / 24) % 59;
-
   return (
     <div className="mx-auto mt-8 flex min-h-[32rem] max-w-[1180px] flex-col gap-10 rounded-b-3xl bg-[#020836] py-16 sm:min-h-[38rem] sm:gap-10 md:min-h-[40rem] md:gap-y-12">
       <div className="flex items-center justify-center gap-2 px-5 text-[4vw] text-gray-50 sm:text-2xl md:text-3xl">
         <TimerIcon size={36} />
-        <h2>زمان شروع مسابقه پیک نوروزی سی تلکام</h2>
+        <h2>زمان {started ? "پایان" : "شروع"} مسابقه پیک نوروزی سی تلکام</h2>
       </div>
-      <div className="mx-auto flex gap-2 text-3xl text-gray-50 sm:text-5xl md:text-6xl">
-        {time}
+      <div className="mx-auto flex select-none gap-2 text-3xl text-gray-50 sm:text-5xl md:text-6xl">
         <TimerPart name="ثانیه" time={seconds} className="hidden sm:flex" />
         <TimeDivider className="hidden sm:flex" />
         <TimerPart name="دقیقه" time={minutes} />
--- lib/consts.ts
+++ lib/consts.ts
@@ -1,3 +1,2 @@
-// export const START = 1710946993691;
-export const START = 1710946993691;
-export const END = "";
+export const START = 1710880200000;
+export const END = 1712089799999;
