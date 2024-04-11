5955b4e704c82c2516e7e1a7d2f7f579f1c40e57
saeed1202 <saeed1202@users.noreply.github.com>
2024-04-11 13:23:16 +0330

    .

--- lib/consts.ts
+++ lib/consts.ts
@@ -6,4 +6,4 @@ export const END = "2024/4/14";
 export const startIn = () => Date.now() > new Date(START).getTime();
 export const endIn = () => differenceInSeconds(addDays(END, 1), Date.now());
 
-export const POINTS = [200, 300, 400, 40, 100, 10, 50, 30];
+export const POINTS = [200, 300, 400, 40, 100, 10, 50, 30,54,44,77,344,75,34,99,33,22,98,37,27];
